dbcb8c5117a5afdc99716b8b56cdd6475e4760ef
diegokbalz8 <diegocabalceta8@gmail.com>
2024-09-18 14:59:59 -0400

    fetching

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,24 +1,35 @@
-let currentIndex = 0;
-const slides = document.querySelectorAll('.slideshow-image');
+const btnSearch = document.getElementById('btnSearch');
 
-function showSlides() {
-  // Hide all slides
-  slides.forEach((slide) => {
-    slide.style.opacity = '0';
-  });
+function searchCondition() {
+    const input = document.getElementById('conditionInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
 
-  // Increment slide index
-  currentIndex++;
-  if (currentIndex > slides.length) {
-    currentIndex = 1;
-  }
+    fetch('travel_recommendation_api.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log('Fetched data:', data);  // Log the entire fetched data
+            const condition = data.countries.find(item => item.name.toLowerCase() === input);
+            console.log('Condition found:', condition);  // Log the found condition
 
-  // Show the current slide
-  slides[currentIndex - 1].style.opacity = '1';
+            if (condition) {
+                const name = condition.name;
+                const cities = condition.cities.join(', ');
+                const treatment = condition.treatment;
+    
+                resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+                resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
 
-  // Change slide every 5 seconds
-  setTimeout(showSlides, 5000);
+                resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${name}</p>`;
+                resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${cities}</p>`;
+                resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+            } else {
+                resultDiv.innerHTML = 'Condition not found.';
+            }
+        }).catch(error => {
+        console.error('Error:', error);
+        resultDiv.innerHTML = 'An error occurred while fetching data.';
+    });
 }
 
-// Start slideshow
-showSlides();
+  btnSearch.addEventListener('click', searchCondition);
